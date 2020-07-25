8c337c94258747acd6b9ec4c68495b9f80a8be2c
Dexkode <dexkode4@gmail.com>
2020-07-25 19:21:10 +0100

    integrated 'X' button

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ fetch(URL)
         person.height,
         person.gender
       );
-      console.log(user);
+      // console.log(user);
       const listItem = document.createElement("li");
       listItem.textContent = person.name;
       listItem.setAttribute("value", index);
       list.appendChild(listItem);
 
       listItem.addEventListener("click", (e) => {
-        console.log(RESULTS[e.target.value].name);
+        // console.log(RESULTS[e.target.value].name);
 
         modal.setAttribute("class", "modal");
         modal.setAttribute("id", "popup-modal");
-        // const modalClose = document.createElement("a");
-        // modalClose.setAttribute("class", "modal-close");
-        // modalClose.textContent = "X";
+        const modalClose = document.createElement("a");
+        modalClose.setAttribute("class", "modal-close");
+        modalClose.textContent = "X";
         const modalImg = document.createElement("div");
         modalImg.setAttribute("class", "modal-img");
         const image = document.createElement("img");
@@ -43,12 +43,13 @@ fetch(URL)
         modal.appendChild(modalContent);
         modalContent.appendChild(modalImg);
         modalImg.appendChild(image);
-        // modalContent.appendChild(modalClose);
+        modalContent.appendChild(modalClose);
 
         modal.style.display = "block";
-        // modalClose.onclick = function () {
-        //   modal.style.display = "none";
-        // };
+        modalClose.onclick = function () {
+          modal.style.display = "none";
+          modal.innerHTML = "";
+        };
 
         const details = document.createElement("div");
         details.setAttribute("class", "details");
